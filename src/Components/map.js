94eb71f69f8cb959b95b03b9284d185a73213498
SISU <danielcoder1022@gmail.com>
2024-06-06 18:44:31 +0900

    zip code

--- src/Components/map.js
+++ src/Components/map.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import reindeer from './reindeer.json';
-import father_christmas from './Father_Christmas.json'
-import { Button, Input } from 'antd'
-import '../index.css'
+import father_christmas from './Father_Christmas.json';
+import { Button, Input } from 'antd';
+import '../index.css';
 
 // Import your custom marker image
 import customMarkerImage from './pin_blue_50.png';
@@ -12,12 +12,13 @@ import customMarkerImage from './pin_blue_50.png';
 const MapComponent = () => {
     const [leafletMap, setLeafletMap] = useState(null);
     const [buildingData, setBuildingData] = useState([]);
-    const[mapshape, setMapShape] = useState('reindeer');
+    const [mapShape, setMapShape] = useState('reindeer');
+    const [zipCode, setZipCode] = useState('');
     const markersRef = useRef([]);
-
+    const [centralcoord, setCentralCoord] = useState([51.47046624769113, -0.06145477294921875]);
 
     useEffect(() => {
-        const map = L.map('map').setView([51.47046624769113,  -0.06145477294921875], 15);
+        const map = L.map('map').setView(centralcoord, 15);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -26,15 +27,17 @@ const MapComponent = () => {
         setLeafletMap(map);
     }, []);
 
-    
     const handleShapeChrismtmas = () => {
         console.log("father");
         setMapShape('father_christmas');
     };
+
     const handleShapeReindeer = () => {
         setMapShape('reindeer');
     };
+
     const [leftdown, setLeftDown] = useState([]);
+    
     useEffect(() => {
         if (leafletMap) {
             const updateBounds = () => {
@@ -58,7 +61,7 @@ const MapComponent = () => {
                 const centerLatitude = (south + north) / 2;
                 const centerLongitude = (west + east) / 2;
                 const radius = 2000;
-                setLeftDown([centerLatitude-0.01, centerLongitude-0.001]);
+                setLeftDown([centerLatitude - 0.01, centerLongitude - 0.001]);
                 return { latitude: centerLatitude, longitude: centerLongitude, radius };
             };
 
@@ -89,45 +92,33 @@ const MapComponent = () => {
 
             const calc_building = () => {
                 const { south, west, north, east } = updateBounds();
-                const area = calculateArea(south, west, north, east);
+                const overpassQuery = (south, west, north, east) => {
+                    const { latitude, longitude, radius } = calculateCenterAndRadius(south, west, north, east);
 
-                if (area < 1104271200000000) {
-                    const overpassQuery = (south, west, north, east) => {
-                        const { latitude, longitude, radius } = calculateCenterAndRadius(south, west, north, east);
+                    return `
+                        [out:json];
+                        (
+                            node["highway"](around:${radius},${latitude},${longitude});
+                            way["highway"](around:${radius},${latitude},${longitude});
+                            relation["highway"](around:${radius},${latitude},${longitude});
+                        );
+                        out body;
+                        >;
+                        out skel qt;
+                    `;
+                };
 
-                        return `
-                            [out:json];
-                            (
-                                node["highway"](around:${radius},${latitude},${longitude});
-                                way["highway"](around:${radius},${latitude},${longitude});
-                                relation["highway"](around:${radius},${latitude},${longitude});
-                            );
-                            out body;
-                            >;
-                            out skel qt;
-                        `;
-                    };
+                const query = overpassQuery(south, west, north, east);
 
-                    const query = overpassQuery(south, west, north, east);
-
-                    fetch('http://overpass-api.de/api/interpreter', {
-                        method: 'POST',
-                        body: query
+                fetch('http://overpass-api.de/api/interpreter', {
+                    method: 'POST',
+                    body: query
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        setBuildingData(extractBuildingInfo(data));
                     })
-                        .then(response => response.json())
-                        .then(data => {
-                            setBuildingData(extractBuildingInfo(data));
-                        })
-                        .catch(error => console.error('Error fetching building data:', error));
-
-                } else {
-                    setBuildingData([]);
-                    leafletMap.eachLayer(layer => {
-                        if (layer instanceof L.Polygon) {
-                            leafletMap.removeLayer(layer);
-                        }
-                    });
-                }
+                    .catch(error => console.error('Error fetching building data:', error));
             }
 
             calc_building();
@@ -135,7 +126,7 @@ const MapComponent = () => {
             // leafletMap.on('zoomend', calc_building);
             // leafletMap.on('moveend', calc_building);
         }
-    }, [leafletMap]);
+    }, [leafletMap, centralcoord]);
 
     useEffect(() => {
         if (leafletMap && buildingData.length > 0) {
@@ -144,7 +135,7 @@ const MapComponent = () => {
             });
             markersRef.current = [];
 
-            const roaddata = mapshape ==='reindeer' ? reindeer : father_christmas;
+            const roaddata = mapShape === 'reindeer' ? reindeer : father_christmas;
             const road_coordinates = [];
             const closest_points = [];
             const original_points = [];
@@ -152,7 +143,7 @@ const MapComponent = () => {
             roaddata.forEach(road_coord => {
                 // Calculate the current road coordinate
                 const currentRoadCoord = [leftdown[0] + road_coord[0] / 40000, leftdown[1] + road_coord[1] / 25000];
-                original_points.push(currentRoadCoord)
+                original_points.push(currentRoadCoord);
                 let minDistance = Infinity;
                 let closestPoint = null;
 
@@ -162,18 +153,19 @@ const MapComponent = () => {
                       element.polygon.forEach(polygondata => {
                           let distance = Math.sqrt(Math.pow(currentRoadCoord[0] - polygondata[0], 2) + Math.pow(currentRoadCoord[1] - polygondata[1], 2));
                           if (distance < minDistance) {
-                            minDistance = distance;
-                            closestPoint = polygondata;
-                        }
+                              minDistance = distance;
+                              closestPoint = polygondata;
+                          }
                       });
                     }
                 });
-                console.log(road_coordinates.length);
+
                 // Add the closest point to closest_points
                 if (closestPoint) {
                     closest_points.push(closestPoint);
                 }
             });
+
             // Define the custom icon
             const customIcon = L.icon({
                 iconUrl: customMarkerImage,
@@ -181,25 +173,48 @@ const MapComponent = () => {
                 iconAnchor: [16, 16], // Point of the icon which will correspond to marker's location
                 popupAnchor: [0, -16] // Point from which the popup should open relative to the iconAnchor
             });
+
             closest_points.forEach(coordPair => {
                 const marker = L.marker(coordPair, { icon: customIcon }).addTo(leafletMap);
                 markersRef.current.push(marker);
             });
         }
-    }, [leafletMap, buildingData, mapshape]);
+    }, [leafletMap, buildingData, mapShape, centralcoord]);
+
+    const handleZipCodeChange = (e) => {
+        setZipCode(e.target.value);
+    };
+
+    const handleZipCodeSubmit = () => {
+        if (zipCode) {
+            fetch(`https://nominatim.openstreetmap.org/search?postalcode=${zipCode}&format=json&addressdetails=1`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data && data.length > 0) {
+                        const { lat, lon } = data[0];
+                        leafletMap.setView([parseFloat(lat), parseFloat(lon)], 15);
+                        setCentralCoord([parseFloat(lat), parseFloat(lon)]);
+                    } else {
+                        console.error('No data found for the provided ZIP code');
+                    }
+                })
+                .catch(error => console.error('Error fetching geocode data:', error));
+        }
+    };
 
     return (
         <>
             <div className="flex mt-2 mb-10 justify-between items-center">
-            <h1 className="text-center font-momo text-6xl bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text animate-text"
-                style={{ marginLeft: 750 }}
+                <h1 className="text-center font-momo text-6xl bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text animate-text"
+                    style={{ marginLeft: 750 }}
                 >
-                Running Map
+                    Running Map
                 </h1>
                 <div className="items-center">
                     <div className="mr-5 my-5">
                         <label>ZIP Code</label>
-                        <Input></Input>
+                        <Input value={zipCode} onChange={handleZipCodeChange} />
+                        <Button onClick={handleZipCodeSubmit} style={{color: '#ffffff', backgroundColor: '#0033cc', marginTop: '5px'}}>Search</Button>
                     </div>
                     <div>
                         <Button className=" mr-2.5" onClick={handleShapeChrismtmas} style={{color: '#ffffff', backgroundColor: '#0033cc'}}>
@@ -213,7 +228,7 @@ const MapComponent = () => {
             </div>
             <div id="map" style={{ height: '700px', width: '100%' }}></div>
         </>
-    )
+    );
 };
 
 export default MapComponent;
